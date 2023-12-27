617944f9cd6e340f6099d74e0799c6ee80849d47
Neha <mneha1394@gmail.com>
2023-12-27 11:48:27 +0530

    updated

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ import os
     
 try: 
     list=[]
-    for page in tqdm(range(1,2)):
+    for page in tqdm(range(1,48)):
         link='https://rategain.com/blog/page/'+ str(page)+'/'
         scraper=cloudscraper.create_scraper()
         res=scraper.get(link)
         soup = BeautifulSoup(res.text,'html.parser')
         list=[]
         for blog in soup.find_all('div',class_='wrap'):
-            image=blog.find('div',class_='img')
-            blogimage=image.find('a').get('href')
-            blogtd=blog.find('div',class_='content')
-            blogtitle=blogtd.find('h6').text
-            blogdate=blogtd.find('span').text
-            likes=blog.find('a',class_='zilla-likes')
-            bloglikes=likes.find('span').text
+            if blog.find('div',class_='img'):
+                image=blog.find('div',class_='img')
+            if image.find('a').get('href'):
+                blogimage=image.find('a').get('href')
+            if blog.find('div',class_='content'):
+                blogtd=blog.find('div',class_='content')
+            if blogtd.find('h6').text:
+                blogtitle=blogtd.find('h6').text
+            if blogtd.find('span').text:
+                blogdate=blogtd.find('span').text
+            if blog.find('a',class_='zilla-likes'):
+                likes=blog.find('a',class_='zilla-likes')
+            if likes.find('span').text:
+                bloglikes=likes.find('span').text
             list.append([blogtitle,blogimage,blogdate,bloglikes])
     df=pd.DataFrame(list,columns=['Blog Title','Blog Image URL','Blog Date','Blog Likes'])
     
@@ -31,7 +38,7 @@ try:
     except OSError:
         pass
     df.to_csv(filename,index=False)
-except:
-    print('Failed to retrieve page')
+finally:
+    print('Completed')
 
 
